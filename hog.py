12f623b0839689b74f54ba864afc8e54fdfe3465
HueInMelody <hueinmelody@outlook.com>
2024-12-20 13:30:35 +0800

    Add files via upload

---
+++ hog.py
@@ -0,0 +1,453 @@
+"""CS 61A Presents The Game of Hog."""
+
+from dice import six_sided, four_sided, make_test_dice
+from ucb import main, trace, interact
+
+GOAL_SCORE = 100  # The goal of Hog is to score 100 points.
+FIRST_101_DIGITS_OF_PI = 31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+
+######################
+# Phase 1: Simulator #
+######################
+
+
+def roll_dice(num_rolls, dice=six_sided):
+    """Simulate rolling the DICE exactly NUM_ROLLS > 0 times. Return the sum of
+    the outcomes unless any of the outcomes is 1. In that case, return 1.
+
+    num_rolls:  The number of dice rolls that will be made.
+    dice:       A function that simulates a single dice roll outcome.
+    """
+    # These assert statements ensure that num_rolls is a positive integer.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls > 0, 'Must roll at least once.'
+    # BEGIN PROBLEM 1
+    total = 0#得到的总分
+    cond = False#是否触发Pig Out
+    for i in range(0,num_rolls):#掷骰子
+        a = dice()
+        total += a
+        if a == 1:
+            cond = True#掷出1，触发Pig Out
+    if cond == True:
+        total = 1#触发Pig Out，得分为1
+    return total#返回得分
+    # END PROBLEM 1
+
+
+def free_bacon(score):
+    """Return the points scored from rolling 0 dice (Free Bacon).
+
+    score:  The opponent's current score.
+    """
+    assert score < 100, 'The game should be over.'
+    pi = FIRST_101_DIGITS_OF_PI
+
+    # Trim pi to only (score + 1) digit(s)
+    # BEGIN PROBLEM 2
+    pi = pi // (10 ** (100 - score))#去尾
+    # END PROBLEM 2
+
+    return pi % 10 + 3
+
+
+def take_turn(num_rolls, opponent_score, dice=six_sided):
+    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
+    Return the points scored for the turn by the current player.
+
+    num_rolls:       The number of dice rolls that will be made.
+    opponent_score:  The total score of the opponent.
+    dice:            A function that simulates a single dice roll outcome.
+    """
+    # Leave these assert statements here; they help check for errors.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
+    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
+    assert opponent_score < 100, 'The game should be over.'
+    # BEGIN PROBLEM 3
+    if num_rolls == 0:#根据num_rolls执行不同函数
+        return free_bacon(opponent_score)
+    else:
+        return roll_dice(num_rolls,dice)
+    # END PROBLEM 3
+
+
+def extra_turn(player_score, opponent_score):
+    """Return whether the player gets an extra turn."""
+    return (pig_pass(player_score, opponent_score) or
+            swine_align(player_score, opponent_score))
+
+
+def swine_align(player_score, opponent_score):
+    """Return whether the player gets an extra turn due to Swine Align.
+
+    player_score:   The total score of the current player.
+    opponent_score: The total score of the other player.
+
+    >>> swine_align(30, 45)  # The GCD is 15.
+    True
+    >>> swine_align(35, 45)  # The GCD is 5.
+    False
+    """
+    # BEGIN PROBLEM 4a
+    for fact in range(min(player_score,opponent_score) + 1,0,-1):#找到最大公约数
+        if player_score % fact == 0 and opponent_score % fact == 0:
+            break
+    if fact >= 10:#判断大小
+        return True
+    else:
+        return False
+    # END PROBLEM 4a
+
+
+def pig_pass(player_score, opponent_score):
+    """Return whether the player gets an extra turn due to Pig Pass.
+
+    player_score:   The total score of the current player.
+    opponent_score: The total score of the other player.
+
+    >>> pig_pass(9, 12)
+    False
+    >>> pig_pass(10, 12)
+    True
+    >>> pig_pass(11, 12)
+    True
+    >>> pig_pass(12, 12)
+    False
+    >>> pig_pass(13, 12)
+    False
+    """
+    # BEGIN PROBLEM 4b
+    if opponent_score - player_score < 3 and opponent_score - player_score > 0:#判断条件
+        return True
+    else:
+        return False
+    # END PROBLEM 4b
+
+
+def other(who):
+    """Return the other player, for a player WHO numbered 0 or 1.
+
+    >>> other(0)
+    1
+    >>> other(1)
+    0
+    """
+    return 1 - who
+
+
+def silence(score0, score1):
+    """Announce nothing (see Phase 2)."""
+    return silence
+
+
+def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
+         goal=GOAL_SCORE, say=silence):
+    """Simulate a game and return the final scores of both players, with Player
+    0's score first, and Player 1's score second.
+
+    A strategy is a function that takes two total scores as arguments (the
+    current player's score, and the opponent's score), and returns a number of
+    dice that the current player will roll this turn.
+
+    strategy0:  The strategy function for Player 0, who plays first.
+    strategy1:  The strategy function for Player 1, who plays second.
+    score0:     Starting score for Player 0
+    score1:     Starting score for Player 1
+    dice:       A function of zero arguments that simulates a dice roll.
+    goal:       The game ends and someone wins when this score is reached.
+    say:        The commentary function to call at the end of the first turn.
+    """
+    who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
+    # BEGIN PROBLEM 5
+    while score0 < goal and score1 < goal:#无人胜出，函数继续
+        if who == 0:#分情况讨论
+            score0 += take_turn(strategy0(score0,score1),score1,dice)
+            mayex = extra_turn(score0,score1)
+        else:
+            score1 += take_turn(strategy1(score1,score0),score0,dice)
+            mayex = extra_turn(score1,score0)
+        if mayex == False:#不触发额外回合
+            who = other(who)#下回合掷骰子玩家改变
+    # END PROBLEM 5
+    # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
+    # BEGIN PROBLEM 6
+        say = say(score0,score1)#每回合更新函数参数
+    # END PROBLEM 6
+    return score0, score1
+
+
+#######################
+# Phase 2: Commentary #
+#######################
+
+
+def say_scores(score0, score1):
+    """A commentary function that announces the score for each player."""
+    print("Player 0 now has", score0, "and Player 1 now has", score1)
+    return say_scores
+
+
+def announce_lead_changes(last_leader=None):
+    """Return a commentary function that announces lead changes.
+
+    >>> f0 = announce_lead_changes()
+    >>> f1 = f0(5, 0)
+    Player 0 takes the lead by 5
+    >>> f2 = f1(5, 12)
+    Player 1 takes the lead by 7
+    >>> f3 = f2(8, 12)
+    >>> f4 = f3(8, 13)
+    >>> f5 = f4(15, 13)
+    Player 0 takes the lead by 2
+    """
+    def say(score0, score1):
+        if score0 > score1:
+            leader = 0
+        elif score1 > score0:
+            leader = 1
+        else:
+            leader = None
+        if leader != None and leader != last_leader:
+            print('Player', leader, 'takes the lead by', abs(score0 - score1))
+        return announce_lead_changes(leader)
+    return say
+
+
+def both(f, g):
+    """Return a commentary function that says what f says, then what g says.
+
+    NOTE: the following game is not possible under the rules, it's just
+    an example for the sake of the doctest
+
+    >>> h0 = both(say_scores, announce_lead_changes())
+    >>> h1 = h0(10, 0)
+    Player 0 now has 10 and Player 1 now has 0
+    Player 0 takes the lead by 10
+    >>> h2 = h1(10, 8)
+    Player 0 now has 10 and Player 1 now has 8
+    >>> h3 = h2(10, 17)
+    Player 0 now has 10 and Player 1 now has 17
+    Player 1 takes the lead by 7
+    """
+    def say(score0, score1):
+        return both(f(score0, score1), g(score0, score1))
+    return say
+
+
+def announce_highest(who, last_score=0, running_high=0):
+    """Return a commentary function that announces when WHO's score
+    increases by more than ever before in the game.
+
+    NOTE: the following game is not possible under the rules, it's just
+    an example for the sake of the doctest
+
+    >>> f0 = announce_highest(1) # Only announce Player 1 score gains
+    >>> f1 = f0(12, 0)
+    >>> f2 = f1(12, 9)
+    9 point(s)! The most yet for Player 1
+    >>> f3 = f2(20, 9)
+    >>> f4 = f3(20, 30)
+    21 point(s)! The most yet for Player 1
+    >>> f5 = f4(20, 47) # Player 1 gets 17 points; not enough for a new high
+    >>> f6 = f5(21, 47)
+    >>> f7 = f6(21, 77)
+    30 point(s)! The most yet for Player 1
+    """
+    assert who == 0 or who == 1, 'The who argument should indicate a player.'
+    # BEGIN PROBLEM 7
+    def say_highest(score0, score1,last_score = last_score,running_high = running_high):
+        if who == 0:#分不同玩家情况判断
+            increasing = score0 - last_score#本回合得分
+            if increasing > running_high:#是否超过历史最高分
+                print("{0} point(s)! The most yet for Player {1}".format(increasing,who))#格式化输出
+                running_high = increasing#更新历史最高分
+            return announce_highest(who,last_score=score0,running_high=running_high)
+        else:
+            increasing = score1 - last_score
+            if increasing > running_high:
+                print("{0} point(s)! The most yet for Player {1}".format(increasing,who))
+                running_high = increasing
+            return announce_highest(who,last_score=score1,running_high=running_high)
+    return say_highest
+    # END PROBLEM 7
+
+
+#######################
+# Phase 3: Strategies #
+#######################
+
+
+def always_roll(n):
+    """Return a strategy that always rolls N dice.
+
+    A strategy is a function that takes two total scores as arguments (the
+    current player's score, and the opponent's score), and returns a number of
+    dice that the current player will roll this turn.
+
+    >>> strategy = always_roll(5)
+    >>> strategy(0, 0)
+    5
+    >>> strategy(99, 99)
+    5
+    """
+    def strategy(score, opponent_score):
+        return n
+    return strategy
+
+
+def make_averaged(original_function, trials_count=1000):
+    """Return a function that returns the average value of ORIGINAL_FUNCTION
+    when called.
+
+    To implement this function, you will have to use *args syntax, a new Python
+    feature introduced in this project.  See the project description.
+
+    >>> dice = make_test_dice(4, 2, 5, 1)
+    >>> averaged_dice = make_averaged(dice, 1000)
+    >>> averaged_dice()
+    3.0
+    """
+    # BEGIN PROBLEM 8
+    def averaged_function(*args):
+        total = 0#累加器
+        for time in range(0,trials_count):
+            total += original_function(*args)
+        return total / trials_count
+    return averaged_function
+    # END PROBLEM 8
+
+
+def max_scoring_num_rolls(dice=six_sided, trials_count=1000):
+    """Return the number of dice (1 to 10) that gives the highest average turn
+    score by calling roll_dice with the provided DICE over TRIALS_COUNT times.
+    Assume that the dice always return positive outcomes.
+
+    >>> dice = make_test_dice(1, 6)
+    >>> max_scoring_num_rolls(dice)
+    1
+    """
+    # BEGIN PROBLEM 9
+    n = 1
+    ave = 0
+    for i in range(1,11):
+        RollDiceAve = make_averaged(roll_dice, trials_count = trials_count)
+        j = RollDiceAve(i,dice)
+        if j > ave:#判断是否大于先前最大值
+            ave = j
+            n = i
+    return n
+    # END PROBLEM 9
+
+
+def winner(strategy0, strategy1):
+    """Return 0 if strategy0 wins against strategy1, and 1 otherwise."""
+    score0, score1 = play(strategy0, strategy1)
+    if score0 > score1:
+        return 0
+    else:
+        return 1
+
+
+def average_win_rate(strategy, baseline=always_roll(6)):
+    """Return the average win rate of STRATEGY against BASELINE. Averages the
+    winrate when starting the game as player 0 and as player 1.
+    """
+    win_rate_as_player_0 = 1 - make_averaged(winner)(strategy, baseline)
+    win_rate_as_player_1 = make_averaged(winner)(baseline, strategy)
+
+    return (win_rate_as_player_0 + win_rate_as_player_1) / 2
+
+
+def run_experiments():
+    """Run a series of strategy experiments and report results."""
+    if True:  # Change to False when done finding max_scoring_num_rolls
+        six_sided_max = max_scoring_num_rolls(six_sided)
+        print('Max scoring num rolls for six-sided dice:', six_sided_max)
+
+    if False:  # Change to True to test always_roll(8)
+        print('always_roll(8) win rate:', average_win_rate(always_roll(8)))
+
+    if False:  # Change to True to test bacon_strategy
+        print('bacon_strategy win rate:', average_win_rate(bacon_strategy))
+
+    if False:  # Change to True to test extra_turn_strategy
+        print('extra_turn_strategy win rate:', average_win_rate(extra_turn_strategy))
+
+    if False:  # Change to True to test final_strategy
+        print('final_strategy win rate:', average_win_rate(final_strategy))
+
+    "*** You may add additional experiments as you wish ***"
+
+
+
+def bacon_strategy(score, opponent_score, cutoff=8, num_rolls=6):
+    """This strategy rolls 0 dice if that gives at least CUTOFF points, and
+    rolls NUM_ROLLS otherwise.
+    """
+    # BEGIN PROBLEM 10
+    bacon_score = free_bacon(opponent_score)#掷0个骰子的得分
+    if bacon_score >= cutoff:
+        return 0
+    else:
+        return num_rolls
+    # END PROBLEM 10
+
+
+def extra_turn_strategy(score, opponent_score, cutoff=8, num_rolls=6):
+    """This strategy rolls 0 dice when it triggers an extra turn. It also
+    rolls 0 dice if it gives at least CUTOFF points and does not give an extra turn.
+    Otherwise, it rolls NUM_ROLLS.
+    """
+    # BEGIN PROBLEM 11
+    if extra_turn(score + free_bacon(opponent_score),opponent_score):#是否额外回合
+        return 0
+    else:
+        return bacon_strategy(score,opponent_score,cutoff,num_rolls)
+    # END PROBLEM 11
+
+
+def final_strategy(score, opponent_score):
+    """Write a brief description of your final strategy.
+
+    *** YOUR DESCRIPTION HERE ***
+    """
+    # BEGIN PROBLEM 12
+    '''always_six_dice = make_averaged(roll_dice, 1000)
+    always_opponent = always_six_dice(4)#计算对方平均得分
+    Emaxnum = max_scoring_num_rolls(six_sided,1000)
+    Emaxscore = always_six_dice(Emaxnum)#计算最大掷骰子平均分
+    if 100 - score > Emaxscore:#分差一回合不能得满时
+        return extra_turn_strategy(score,opponent_score,always_opponent,Emaxnum)
+    else:#分差可能一回合得满时
+        for first_num in range(0,11):#计算平均期望高于分差的第一个骰子数
+            if take_turn(first_num, opponent_score) >= 100 - score:
+                break
+        return extra_turn_strategy(score,opponent_score,100 - score,first_num)
+    '''
+
+    #测试的时候显示这个函数不允许调用max_scoring_num_rolls，直接搬函数体代码又显示超时.....于是把上面那段代码注释掉了。保持原样吧
+    #我还觉得我的算法挺妙的啊........
+    return 6
+    # END PROBLEM 12
+
+##########################
+# Command Line Interface #
+##########################
+
+# NOTE: Functions in this section do not need to be changed. They use features
+# of Python not yet covered in the course.
+
+
+@main
+def run(*args):
+    """Read in the command-line argument and calls corresponding functions."""
+    import argparse
+    parser = argparse.ArgumentParser(description="Play Hog")
+    parser.add_argument('--run_experiments', '-r', action='store_true',
+                        help='Runs strategy experiments')
+
+    args = parser.parse_args()
+
+    if args.run_experiments:
+        run_experiments()
